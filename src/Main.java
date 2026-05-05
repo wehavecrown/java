d343d03283b49d97b4dadd0c8f2e8795d97ba31d
Adelani <wehavecrown@gmail.com>
2026-05-05 05:21:44 +0100

    MADLIBS

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        // Calculate area of a rectangle
-
-        double width = 0;
-        double length = 0;
-        double area = 0;
+        //MAD LIBS GAME
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is the width? ");
-        width = scanner.nextDouble();
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        System.out.print("What is the length? ");
-        length = scanner.nextDouble();
+        System.out.print("Enter an adjective: ");
+        adjective1 = scanner.nextLine();
 
-        area = width * length;
+        System.out.print("Enter a noun: ");
+        noun1 = scanner.nextLine();
 
-        System.out.println("The area of the rectangle is : " + area + "cm²");
+        System.out.print("Enter an adjective: ");
+        adjective2 = scanner.nextLine();
+
+        System.out.print("Enter a verb end with -ing: ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("\nToday i went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, i saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
 
         scanner.close();
     }

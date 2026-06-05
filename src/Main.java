1dc74faa0d0ba647795b457cc3d20dcb7b26fa71
Adelani <wehavecrown@gmail.com>
2026-06-05 10:32:17 +0100

    active

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        //OPERATORS
 
+        int num1 = 7;
+        int num2 = 5;
 
-        Scanner scanner = new Scanner(System.in);
-
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
-
-        //SHOPPING CART
-
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
-
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
-
-        System.out.print("How many would you like?: ");
-        quantity = scanner.nextInt();
-
-        total = price * quantity;
-
-        System.out.println("\nYou bought " + quantity + " " + item + "/s");
-        System.out.println("Your total is " + currency + total);
-
-
-        scanner.close();
+        int result = num1 + num2;
+        System.out.println(result);
     }
 
 }

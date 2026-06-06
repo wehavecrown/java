42f7a14bc3efa78a5958e8cf32057f15a6941dba
Adelani <wehavecrown@gmail.com>
2026-06-06 05:04:11 +0100

    SWITCH UP

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //OPERATORS
+        int n = 2;
 
-        int num1 = 7;
-        int num2 = 5;
+        // SWITCH
 
-        int result = num1 + num2;
-        System.out.println(result);
+        switch(n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
     }
-
 }

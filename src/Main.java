8268ad06750da7d579c74facf514b06626e73589
Adelani <wehavecrown@gmail.com>
2026-06-08 09:18:09 +0100

    NESTED

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int n = 2;
+        // NESTED WHILE LOOP
 
-        // SWITCH
+        int i = 1;
 
-        switch(n) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
+        while (i <= 4) {
+            System.out.println("Hi " + i);
+
+            int j = 1;
+            while (j <= 3) {
+                System.out.println("Hello " + j);
+                j++;
+            }
+
+            i++;
         }
+
+        System.out.println("the value of i is: " + i);
     }
 }
+

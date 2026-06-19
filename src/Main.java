4c3661494cbdd159c5039cd9fe4776460c9ce318
Adelani <wehavecrown@gmail.com>
2026-06-19 01:18:53 +0100

    forloop2.0

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
+class Student {
+    int rollno;
+    String name;
+    int marks;
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int nums[][] = new int[3][4];
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Navin";
+        s1.marks = 88;
 
-        for (int i = 0; i < nums.length; i++){
-            for (int j = 0; j < nums[i].length; j++){
-                nums[i][j] = (int)(Math.random() * 10);
-            }
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "Harsh";
+        s2.marks = 67;
+
+        Student s3 = new Student();
+        s3.rollno = 1;
+        s3.name = "Kiran";
+        s3.marks = 99;
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (Student student : students){
+            System.out.println(student.name + " : " + student.marks);
         }
 
-        for (int n[] : nums){
-            for (int m: n){
-                System.out.print(m + " ");
-            }
-            System.out.println();
-        }
     }
 }

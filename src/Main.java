c1e91011f0133a6d49b1b0c895f36ea4795427c2
Adelani <wehavecrown@gmail.com>
2026-06-23 08:01:03 +0100

    constr

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,11 @@ class Human {
         name = "John";
     }
 
+    public Human(int age, String nanme) {
+        this.age = age;
+        this.name = nanme;
+    }
+
     public int getAge() {
         return age;
     }
@@ -30,10 +35,13 @@ public class Main {
     public static void main(String[] args) {
 
         Human obj = new Human();
+        Human obj1 = new Human(18, "John");
 
         obj.setAge(11);
         obj.setName("Navin");
 
         System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
+
     }
 }

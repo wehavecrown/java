7020744e5e02243bb6b2e1bba4dbe5bc5843a7c5
Adelani <wehavecrown@gmail.com>
2026-06-23 07:39:23 +0100

    Constructor

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,39 @@
-class Student {
-    int rollno;
-    String name;
-    int marks;
+class Human {
+
+    private int age;
+    private String name;
+
+    public Human() {
+        age = 12;
+        name = "John";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Student s1 = new Student();
-        s1.rollno = 1;
-        s1.name = "Navin";
-        s1.marks = 88;
+        Human obj = new Human();
 
-        Student s2 = new Student();
-        s2.rollno = 2;
-        s2.name = "Harsh";
-        s2.marks = 67;
+        obj.setAge(11);
+        obj.setName("Navin");
 
-        Student s3 = new Student();
-        s3.rollno = 1;
-        s3.name = "Kiran";
-        s3.marks = 99;
-
-        Student students[] = new Student[3];
-        students[0] = s1;
-        students[1] = s2;
-        students[2] = s3;
-
-        for (Student student : students){
-            System.out.println(student.name + " : " + student.marks);
-        }
-
+        System.out.println(obj.getName() + " : " + obj.getAge());
     }
 }
